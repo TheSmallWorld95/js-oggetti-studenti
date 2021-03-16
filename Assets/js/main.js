fa538248156ff28dc56f306e8e8507697c34914f
Fabio-Colombo <thesmallworld95@gmail.com>
2021-03-16 17:53:09 +0100

    Completo mancano le rifiniture

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -12,15 +12,24 @@ var studenteA = {
   cognome: "Colombo",
   età: "25 anni",
 }
-
+var stringa = "";
 // Attraverso un ciclo(for-in) che si usa per gli oggetti
   // Stampo con .text le proprietà (tutte) come fosse una C.I.
-for (var proprietà in studenteA) {
-console.log(proprietà, studenteA[proprietà]);
+for (var caratteristiche in studenteA) {
+console.log(caratteristiche, studenteA[caratteristiche]);
+// (mi riscrive tutte i valori di un oggetto)
+// stringa += studenteA[caratteristiche] + " ";
 }
-$("#nome").text(studenteA.nome);
-$("#cognome").text(studenteA.cognome);
-$("#età").text(studenteA.età);
+
+// (Altro metodo per scrivere i valori presi singolarmente di un oggetto)
+// stringa = studenteA.nome + " " + studenteA.cognome + " " + studenteA.età;
+// $("#prova").text(stringa);
+
+$(".studente1 .nome").text(studenteA.nome);
+$(".studente1 .cognome").text(studenteA.cognome);
+$(".studente1 .età").text(studenteA.età);
+
+
 
 // Creo un array [] che contine più variabili(oggetto)
 // che descrivano n studenti
@@ -42,8 +51,32 @@ var classe = [
 for (var i = 0; i < classe.length; i++) {
   console.log(classe[i].nome + " " + classe[i].cognome);
 }
+
+$(".studente2 .nome").text(classe[0].nome);
+$(".studente2 .cognome").text(classe[0].cognome);
+$(".studente2 .età").text(classe[0].età);
+
+$(".studente3 .nome").text(classe[1].nome);
+$(".studente3 .cognome").text(classe[1].cognome);
+$(".studente3 .età").text(classe[1].età);
+
 // Attraverso un input-form sull'HTML invio(bottone al click)
 // le info che l'utente inserisce nel form
 //  riguardo 3 nuovi studenti(variabile-oggetto)
+$("button").click(function () {
+  var nome = $("input[name = nome]").val()
+  var cognome = $("input[name = cognome]").val()
+  var anni = $("input[name = età]").val()
+
+  var studente = {
+    nome: nome,
+    cognome: cognome,
+    età: anni,
+  };
+
+  classe.push(studente);
+  console.log(classe);
+
+})
   // Tramite push dopo il click
   // le inserisco nella variabile-oggetto creata precedentemente
